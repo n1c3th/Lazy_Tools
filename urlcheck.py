0c6ec3337e2d87ef4053af18fc56fade448e86b1
Happyter <62225971+n1c3th@users.noreply.github.com>
2023-07-29 20:17:47 +0800

    Update urlcheck.py
    add the code

--- urlcheck.py
+++ urlcheck.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
+#This code takes a URLs as input from a file and returns the status code of those URLs.
 
+import requests as req
+
+def urlread():
+    try:
+        filename = input("Please enter the file name: ")
+
+        with open(filename, "r") as f:
+            for url in f.readlines():
+                resp = req.get(url.strip())
+                code = resp.status_code
+                print("Status code for " + url.strip() + ": " + str(code))
+    except requests.exceptions.HTTPError as e:
+        print("Error: " + str(e))
+
+urlread()
